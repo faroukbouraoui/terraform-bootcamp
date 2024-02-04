3a43918c72e7369f22cb3c3569c1b316c1443b1b
farouk <49481558+faroukbouraoui@users.noreply.github.com>
2024-02-04 17:34:51 +0100

    Create python-script.py

---
+++ bin/python-script.py
@@ -0,0 +1,8 @@
+import pandas as pd
+# create a DataFrame
+data = {'name': ['John', 'Jane', 'Sam'],
+ 'age': [32, 28, 45],
+ 'city': ['New York', 'London', 'Paris']}
+df = pd.DataFrame(data)
+# print the DataFrame
+print
